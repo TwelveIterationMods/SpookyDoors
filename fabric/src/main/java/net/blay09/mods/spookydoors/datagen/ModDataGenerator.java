e2348c1c3ba94bf768c49b3d801f74cbe12ad4e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-05 16:37:24 +0100

    fix: Add missing generation of loot tables

--- fabric/src/main/java/net/blay09/mods/spookydoors/datagen/ModDataGenerator.java
+++ fabric/src/main/java/net/blay09/mods/spookydoors/datagen/ModDataGenerator.java
@@ -9,6 +9,7 @@ public class ModDataGenerator implements DataGeneratorEntrypoint {
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         final var pack = fabricDataGenerator.createPack();
 
+        pack.addProvider(SpookyDoorsLootTableProvider::new);
         pack.addProvider(SpookyDoorsModelProvider::new);
         pack.addProvider(SpookyDoorsBlockTagsProvider::new);
         pack.addProvider(SpookyDoorsItemTagsProvider::new);
