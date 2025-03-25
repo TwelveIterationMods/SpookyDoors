c0c9ed667498f339679d787152ce93ef7bec5187
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-25 07:35:30 +0100

    feat: Update to Minecraft 1.21.5

--- common/src/main/java/net/blay09/mods/spookydoors/client/ModRenderers.java
+++ common/src/main/java/net/blay09/mods/spookydoors/client/ModRenderers.java
@@ -4,8 +4,10 @@ import net.blay09.mods.balm.api.client.rendering.BalmRenderers;
 import net.blay09.mods.spookydoors.ModBlockEntities;
 import net.blay09.mods.spookydoors.client.render.SpookyDoorBlockEntityRenderer;
 
+import static net.blay09.mods.spookydoors.SpookyDoors.id;
+
 public class ModRenderers {
     public static void initialize(BalmRenderers renderers) {
-        renderers.registerBlockEntityRenderer(ModBlockEntities.spookyDoor::get, SpookyDoorBlockEntityRenderer::new);
+        renderers.registerBlockEntityRenderer(id("spooky_door"), ModBlockEntities.spookyDoor::get, SpookyDoorBlockEntityRenderer::new);
     }
 }
