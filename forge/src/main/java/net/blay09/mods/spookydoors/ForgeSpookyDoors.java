2bc0ce0452c680898bd15f6e7af80aa05877cdc4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-30 08:40:48 +0200

    fix: Fix crash on Forge due to wrong load context

--- forge/src/main/java/net/blay09/mods/spookydoors/ForgeSpookyDoors.java
+++ forge/src/main/java/net/blay09/mods/spookydoors/ForgeSpookyDoors.java
@@ -1,19 +1,21 @@
 package net.blay09.mods.spookydoors;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.blay09.mods.balm.api.client.BalmClient;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.blay09.mods.spookydoors.client.SpookyDoorsClient;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(SpookyDoors.MOD_ID)
 public class ForgeSpookyDoors {
 
-    public ForgeSpookyDoors() {
-        Balm.initialize(SpookyDoors.MOD_ID, EmptyLoadContext.INSTANCE, SpookyDoors::initialize);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(SpookyDoors.MOD_ID, EmptyLoadContext.INSTANCE, SpookyDoorsClient::initialize));
+    public ForgeSpookyDoors(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(SpookyDoors.MOD_ID, loadContext, SpookyDoors::initialize);
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(SpookyDoors.MOD_ID, loadContext, SpookyDoorsClient::initialize));
     }
 
 }
