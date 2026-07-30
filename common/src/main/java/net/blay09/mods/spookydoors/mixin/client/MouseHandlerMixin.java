7039b4fd8982f6b2143c74a79a3390c8368b693e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-07-30 17:54:28 +0200

    refactor: Move MouseHandlerMixin to client package

--- common/src/main/java/net/blay09/mods/spookydoors/mixin/client/MouseHandlerMixin.java
+++ common/src/main/java/net/blay09/mods/spookydoors/mixin/client/MouseHandlerMixin.java
@@ -1,4 +1,4 @@
-package net.blay09.mods.spookydoors.mixin;
+package net.blay09.mods.spookydoors.mixin.client;
 
 import net.blay09.mods.spookydoors.client.SpookyDoorsClient;
 import net.minecraft.client.MouseHandler;
