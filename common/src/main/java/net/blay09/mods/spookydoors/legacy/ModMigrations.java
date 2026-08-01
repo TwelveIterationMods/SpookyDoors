30561c48079002640a8aaa4eca5980a181a5414e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-01 12:53:00 +0200

    fix: Fix migration failing if other mods register wood types

--- common/src/main/java/net/blay09/mods/spookydoors/legacy/ModMigrations.java
+++ common/src/main/java/net/blay09/mods/spookydoors/legacy/ModMigrations.java
@@ -4,20 +4,35 @@ import net.blay09.mods.balm.api.Balm;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.properties.WoodType;
+
+import java.util.List;
 
 import static net.blay09.mods.spookydoors.SpookyDoors.id;
 
 public class ModMigrations {
+    private static final List<String> LEGACY_DOOR_TYPES = List.of(
+            "oak",
+            "spruce",
+            "birch",
+            "acacia",
+            "cherry",
+            "jungle",
+            "dark_oak",
+            "mangrove",
+            "bamboo",
+            "crimson",
+            "warped"
+    );
+
     public static void initialize() {
-        WoodType.values().forEach(woodType -> {
-            addDoorAlias(BuiltInRegistries.BLOCK, woodType);
-            addDoorAlias(BuiltInRegistries.ITEM, woodType);
+        LEGACY_DOOR_TYPES.forEach(doorType -> {
+            addDoorAlias(BuiltInRegistries.BLOCK, doorType);
+            addDoorAlias(BuiltInRegistries.ITEM, doorType);
         });
     }
 
-    private static <T> void addDoorAlias(Registry<T> registry, WoodType woodType) {
-        String doorName = woodType.name() + "_door";
+    private static <T> void addDoorAlias(Registry<T> registry, String doorType) {
+        final var doorName = doorType + "_door";
         Balm.getRegistries().addAlias(registry, id("spooky_" + doorName), new ResourceLocation(doorName));
     }
 }
