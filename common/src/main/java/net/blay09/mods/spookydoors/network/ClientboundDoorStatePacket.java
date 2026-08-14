bc3cd06e04987fa677b81d14a0d589249bc725aa
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-14 08:43:19 +0200

    fix: Fix possible desync when door state arrives before chunk data is available

--- common/src/main/java/net/blay09/mods/spookydoors/network/ClientboundDoorStatePacket.java
+++ common/src/main/java/net/blay09/mods/spookydoors/network/ClientboundDoorStatePacket.java
@@ -3,7 +3,6 @@ package net.blay09.mods.spookydoors.network;
 import net.blay09.mods.spookydoors.SpookyDoors;
 import net.blay09.mods.spookydoors.core.ClientSpookyDoor;
 import net.blay09.mods.spookydoors.core.SpookyDoorProvider;
-import net.blay09.mods.spookydoors.util.SpookyDoorUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
@@ -30,14 +29,11 @@ public record ClientboundDoorStatePacket(BlockPos pos, float openness, boolean s
 
     public static void handle(Player player, ClientboundDoorStatePacket message) {
         final var level = player.level();
-        final var state = level.getBlockState(message.pos);
-        if (SpookyDoorUtils.isSupportedDoor(state)) {
-            final var door = SpookyDoorProvider.get(level).of(message.pos, state);
-            door.spooky(message.spooky);
-            final var isLocallyControlled = door instanceof ClientSpookyDoor clientSpookyDoor && clientSpookyDoor.locallyControlled();
-            if (!isLocallyControlled) {
-                door.percentOpen(message.openness);
-            }
+        final var door = SpookyDoorProvider.get(level).at(message.pos);
+        door.spooky(message.spooky);
+        final var isLocallyControlled = door instanceof ClientSpookyDoor clientSpookyDoor && clientSpookyDoor.locallyControlled();
+        if (!isLocallyControlled) {
+            door.percentOpen(message.openness);
         }
     }
 
