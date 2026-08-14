128edae32417c7b1233eecfac55b07ce65d586e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-14 09:18:34 +0200

    fix(dev): Make moveCameraWithDoor option unsynced

--- common/src/main/java/net/blay09/mods/spookydoors/SpookyDoorsConfig.java
+++ common/src/main/java/net/blay09/mods/spookydoors/SpookyDoorsConfig.java
@@ -19,7 +19,6 @@ public class SpookyDoorsConfig {
     @Comment("Allows players to use items in the spookydoors:haunts_doors tag on non-spooky doors to make them spooky. By default, this is a ghast tear. This has no effect when Spooky Door Activation is FORCED.")
     public boolean allowItemToHauntDoors = true;
 
-    @Synced
     @Comment("Turns the player's camera while dragging a spooky door open or closed, to avoid immersion-breaking hard stops.")
     public boolean moveCameraWithDoor = true;
 
