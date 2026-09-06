060a26f3deb2a14f66201aa7442a80664ebd8ebf
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-09-06 12:24:40 +0200

    fix: Explicitly sync spooky doors even if they were not tracked before #15

--- common/src/main/java/net/blay09/mods/spookydoors/util/SpookyDoorUtils.java
+++ common/src/main/java/net/blay09/mods/spookydoors/util/SpookyDoorUtils.java
@@ -2,9 +2,13 @@ package net.blay09.mods.spookydoors.util;
 
 import net.blay09.mods.spookydoors.block.ModBlockTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DoorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+import java.util.function.BiConsumer;
 
 public class SpookyDoorUtils {
     public static boolean isSupportedDoor(BlockState state) {
@@ -25,5 +29,33 @@ public class SpookyDoorUtils {
 
     public static float getDefaultOpenness(BlockState state) {
         return state.hasProperty(DoorBlock.OPEN) && state.getValue(DoorBlock.OPEN) ? 1f : 0f;
+    }
+
+    public static void forEachSupportedDoor(Level level, LevelChunk chunk, BiConsumer<BlockPos, BlockState> consumer) {
+        final var chunkPos = chunk.getPos();
+        final var minX = chunkPos.getMinBlockX();
+        final var minZ = chunkPos.getMinBlockZ();
+        final var sections = chunk.getSections();
+        for (var sectionIndex = 0; sectionIndex < sections.length; sectionIndex++) {
+            final var section = sections[sectionIndex];
+            if (section == null || section.hasOnlyAir() || !section.maybeHas(SpookyDoorUtils::isSupportedDoor)) {
+                continue;
+            }
+
+            final var minY = level.getSectionYFromSectionIndex(sectionIndex) << 4;
+            for (var localY = 0; localY < 16; localY++) {
+                for (var localZ = 0; localZ < 16; localZ++) {
+                    for (var localX = 0; localX < 16; localX++) {
+                        final var state = section.getBlockState(localX, localY, localZ);
+                        if (isSupportedDoor(state)) {
+                            final var pos = new BlockPos(minX + localX, minY + localY, minZ + localZ);
+                            if (getBasePos(pos, state).equals(pos)) {
+                                consumer.accept(pos, state);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
